d71060e77a5b0130cd12398125f4dbf8a51b462e
Wesley Kerfoot <wjak56@gmail.com>
2021-10-17 00:17:07 -0400

    reorganize stuff

--- flash.sh
+++ flash.sh
@@ -1,12 +1,11 @@
 nodemcu-tool reset
 esptool.py erase_flash
-esptool.py --port /dev/ttyUSB0 write_flash -fm qio 0x00000 nodemcu-release-16-modules-2021-10-09-22-33-53-float.bin
+esptool.py --port /dev/ttyUSB0 write_flash -fm qio 0x00000 firmware/nodemcu-release-16-modules-2021-10-09-22-33-53-float.bin
 
-
-nodemcu-tool upload *.lua 
+nodemcu-tool upload *.lua libs/*
 
 while [[ $? != 0 ]]; do
-  nodemcu-tool upload *.lua 
+  nodemcu-tool upload *.lua libs/*
 done
 
 echo 'dofile("init.lua")' | nodemcu-tool terminal
